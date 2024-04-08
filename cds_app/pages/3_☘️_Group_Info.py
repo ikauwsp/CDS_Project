20802a00b1027f3d388d7a8677970db9fc03a86f
ikauwsp <ikau12345@gmail.com>
2024-04-08 19:01:23 +0800

    minor fixes

--- cds_app/pages/3_☘️_Group_Info.py
+++ cds_app/pages/3_☘️_Group_Info.py
@@ -6,7 +6,7 @@ class group_info():
     st.header('CDS Group 15')
     st.write('***Insert Group Photo here***')
     st.markdown('''
-    Group Members:
+    Our Group Members:
     - Wongsaphat Pipatsawettanan (1006297)
     - Mohamed Ansar Ahmed (1006015)
     - Tan Pheng Yuan Ryner (1005982)
